1965025a333a2093f29857a642c72aee259313ca
zeal <1945806168@qq.com>
2019-05-12 13:55:01 +0800

    Add a file jsonp

---
+++ src/main/java/com/jt/Starter01.java
@@ -0,0 +1,11 @@
+package com.jt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Starter01 {
+	public static void main(String[] args) {
+		SpringApplication.run(Starter01.class, args);
+	}
+}
